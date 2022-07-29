bf08ac8f510912c7687706ef2a57d889dadc3584
Fabian5150 <fabian.strauch.04@web.de>
2022-07-30 00:13:19 +0200

    'claimable field' for tic tac toe

--- client/src/components/Column.js
+++ client/src/components/Column.js
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
-import { Grid } from "semantic-ui-react"
+import { Grid, Icon } from "semantic-ui-react"
 
 const MyColumn = (key) => {
-  const [color, setColor] = useState('green')
+  const [color, setColor] = useState('grey')
+  const [icon, setIcon] = useState("square")
 
-  const changeColor = () => {
-    if(color === "green"){
-      setColor("red")
-    } else setColor('green')
+  const claimField = () => {
+    if(color !== "grey"){
+      return alert("field is already claimed, choose another one!")
+    }
+
+    setColor("red")
+    setIcon("close")
   }
 
   return (
-    <Grid.Column color={color} key={key} onClick={() => changeColor()}>⚫</Grid.Column>
+    <Grid.Column color={color} key={key} onClick={() => claimField()}>
+      <Icon name={icon} size="massive" />
+    </Grid.Column>
   )
 }
 
